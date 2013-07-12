7904f7068fa87445a8baa7d4736fe48ac68a513e
Jace Poirier-Pinto <jace@Jacezilla.(none)>
2013-07-12 14:44:24 -0700

    init

---
+++ root/Gruntfile.js
@@ -0,0 +1,102 @@
+module.exports = function( grunt ) {
+
+	// Project configuration
+	grunt.initConfig( {
+		pkg:    grunt.file.readJSON( 'package.json' ),
+		concat: {
+			options: {
+				stripBanners: true,
+				banner: '/*! <%= pkg.title %> - v<%= pkg.version %> - <%= grunt.template.today("yyyy-mm-dd") %>\n' +
+					' * <%= pkg.homepage %>\n' +
+					' * Copyright (c) <%= grunt.template.today("yyyy") %>;' +
+					' * Licensed GPLv2+' +
+					' */\n'
+			},
+			{%= js_safe_name %}: {
+				src: [
+					'assets/js/{%= js_safe_name %}.js'
+				],
+				dest: 'assets/js/{%= js_safe_name %}.js'
+			}
+		},
+		jshint: {
+			all: [
+				'Gruntfile.js',
+				'assets/js/**/*.js',
+				'assets/js/test/**/*.js'
+			],
+			options: {
+				curly:   true,
+				eqeqeq:  true,
+				immed:   true,
+				latedef: true,
+				newcap:  true,
+				noarg:   true,
+				sub:     true,
+				undef:   true,
+				boss:    true,
+				eqnull:  true,
+				globals: {
+					exports: true,
+					module:  false
+				}
+			}
+		},
+		uglify: {
+			all: {
+				files: {
+					'assets/js/{%= js_safe_name %}.min.js': ['assets/js/{%= js_safe_name %}.js']
+				},
+				options: {
+					banner: '/*! <%= pkg.title %> - v<%= pkg.version %> - <%= grunt.template.today("yyyy-mm-dd") %>\n' +
+						' * <%= pkg.homepage %>\n' +
+						' * Copyright (c) <%= grunt.template.today("yyyy") %>;' +
+						' * Licensed GPLv2+' +
+						' */\n',
+					mangle: {
+						except: ['jQuery']
+					}
+				}
+			}
+		},
+		test:   {
+			files: ['assets/js/test/**/*.js']
+		},
+		compass: {
+      dist: {
+        options: {
+          config: 'config.rb',
+          force: true
+        }
+      }
+    },
+		watch:  {
+			options: {
+        livereload: true,
+      },
+      compass: {
+        files: ['assets/scss/**/*.{scss,sass}'],
+        tasks: ['compass']
+      },
+			scripts: {
+				files: ['assets/js/**/*.js', 'assets/js/foundation/**/*.js', 'assets/js/vendor/**/*.js'],
+				tasks: ['jshint', 'concat', 'uglify'],
+				options: {
+					debounceDelay: 500
+				}
+			}
+		}
+	} );
+
+	// Load other tasks
+	grunt.loadNpmTasks('grunt-contrib-jshint');
+	grunt.loadNpmTasks('grunt-contrib-concat');
+	grunt.loadNpmTasks('grunt-contrib-uglify');
+	grunt.loadNpmTasks('grunt-contrib-compass');
+	grunt.loadNpmTasks('grunt-contrib-watch');
+
+	// Default task.
+	grunt.registerTask( 'default', ['watch'] );
+
+	grunt.util.linefeed = '\n';
+};
